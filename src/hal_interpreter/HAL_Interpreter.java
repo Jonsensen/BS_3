fde94172da80cabd93eca96b0ef9b8cac48f163e
JF <>
2015-05-16 15:33:35 +0200

    -Konsolenaufruf mit Dateinamen ergänzt - ACC erstellt -ALU erstellt +startExec + ExecComm ..

--- src/hal_interpreter/HAL_Interpreter.java
+++ src/hal_interpreter/HAL_Interpreter.java
@@ -1,25 +1,50 @@
 package hal_interpreter;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-// Test...
+/*
+NEW:
+-listelement.wholecommand ergänzt
+- Übergabe der Auszuführenden Datei in Console möglich 
+-Acc erstellt
+-ALU erstellt + Konstruktor + ExecComm + startExec
+- ProgrammMemory.getlistElement hinzugefügt
+*/
 public class HAL_Interpreter {
 
-    
+  
     
     
     public static void main(String[] args) throws IOException {
        
-        File input = new File("test1.txt");  // Test Datein = test.txt,test1.txt
-        ProgramMemory test = new ProgramMemory(input);
-        System.out.println(test.getInstruktion(12));
+        String defautFileName ="test.txt";
+        boolean debugMode = false;
+        File input = new File(defautFileName);
+        
+        // Funktioniert ohne debug -> erweitern 
+       // Übergabe des Programmnamens im ersten Parameter
+        if(args.length==1){
+            input = new File(args[0]);
+            System.out.println("Einhabedatei:"+args[0]);
+        }
+         // Ohne Parameter: default Datei setzen
+        else{
+            input = new File("test.txt");  // Test Datein = test.txt,test1.txt 
+        }
+        
+        
+        
+       
+        ProgramMemory ProgrammMem = new ProgramMemory(input);
+        Register regs = new Register();
+        ALU alu = new ALU(ProgrammMem,regs,debugMode);
+        System.out.println("Ausgabe der Instruktion in Main: "+ProgrammMem.getInstruktion(3));
+      alu.startExec();
         
     }
     
 }
-
 
 /* ProgrammMemory Testen
         File input = new File("test1.txt");
