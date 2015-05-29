1d69cc92f3b75e0a962d57079ae894182a8ad966
debian <debian@debian>
2015-05-29 22:02:15 +0200

    Main ausgelagert und HAL_Interpreter als Thread

--- src/hal_interpreter/HAL_Interpreter.java
+++ src/hal_interpreter/HAL_Interpreter.java
@@ -3,92 +3,43 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class HAL_Interpreter {
+public class HAL_Interpreter extends Thread{
 
+   private ALU alu;
   
+    // Konstruktor des HAl Interpreters - zusammensetzen der Bestandsteile
+    public HAL_Interpreter(int anzINs, int anzOUTs,File input,boolean debug)throws IOException{
     
-    public static void main(String[] args) throws IOException {
-       // Zum Aufruf des Programmes im Debug modus "-d" angeben !
-        String defautFileName ="test.txt";
-        boolean debugMode = false; 
         
-        File input = new File(defautFileName);
-        // Später(Aufgabe 3) in Konstruktor von HAL_Interpreter anzugeben 
-        int anzahlINIOs= 5;
-        int anzahlOUTIOs = 5;
+    ProgramMemory ProgrammMem = new ProgramMemory(input);
+    
+    Register regs = new Register();
         
-        // Default namen ohne Debug Infos
-        if (args.length==0){
-        input = new File(defautFileName);
-        }
+        IN[] INS = new IN[anzINs]; 
+        OUT[] OUTS = new OUT[anzOUTs];
         
-        if (args.length==1){// Entweder andere Datei Ohne Debug Infos oder nur Debug Infos
-            
-          
-            if (args[0].equals("-d")){// DefaultFileName mit Debug Infos
-                debugMode=true;
-                input = new File (defautFileName);
-                }
-            
-            
-            else{// andere Datei ohne Debug Infos
-                input = new File(args[0]);
-                }
-            
-        }
-         
-        
-        //anderer Dateinamen und Debug Infos
-        else if (args.length==2){
-        input = new File (args[0]);
-        debugMode=true;
-        }
-            
-        // Erzeugen der HAL Komponenten:
-        ProgramMemory ProgrammMem = new ProgramMemory(input);
-        Register regs = new Register();
-        
-        IN[] INS = new IN[anzahlINIOs]; 
-        OUT[] OUTS = new OUT[anzahlOUTIOs];
-        
-        for (int i=0;i<anzahlINIOs;i++){
+        // Init INPUT IO
+        for (int i=0;i<anzINs;i++){
             INS[i]= new IN();
         }
-        
-        for (int i=0;i<anzahlOUTIOs;i++){
+        // Init OUTPUT IO
+        for (int i=0;i<anzOUTs;i++){
             OUTS[i]= new OUT();
         }
         
+         alu = new ALU(ProgrammMem,regs,debug,INS,OUTS);
         
-        ALU alu = new ALU(ProgrammMem,regs,debugMode,INS,OUTS);
-       // System.out.println("Ausgabe der Instruktion in Main: "+ProgrammMem.getInstruktion(0));
-        alu.startExec();
+        }
+
+   // Erzeugen des Neuen Threads
+    public void run() {
+      //alu.startExec();
+       // Hier auszuführender Code
         
     }
     
+
+ 
+    
+    
 }
-
-/* ProgrammMemory Testen
-        File input = new File("test1.txt");
-        ProgramMemory test = new ProgramMemory(input);
-        System.out.println(test.getInstruktion(12));
-
-*/
-
-/* Register testen
-
- Register r= new Register();
-        
-        r.setregister(0, 100);
-        r.setregister(3, 130);
-        r.setregister(6, 180);
-        r.setregister(9, 200);
-        r.setregister(20, 1); //muss fehlschlagen
-        
-        
-        for(int i=0;i<10;i++){
-        
-        System.out.println("Register:"+i+":"+r.getregister(i));
-        
-        }
-*/
