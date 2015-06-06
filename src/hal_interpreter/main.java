5924c97f074b1ca6568187925cc8acf4f83ed82c
debian <debian@debian>
2015-06-06 14:24:02 +0200

    Main funktion schlank gemacht.

--- src/hal_interpreter/main.java
+++ src/hal_interpreter/main.java
@@ -1,18 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ TO DO : Schönhetitsänderungen: 
+-IO Interface nocht als Klasse sondern als Interface 
+- Schleifen, die Arrays komplett durchgehen durch Iteratoren ersetzen! 
  */
 
 package hal_interpreter;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Arrays;
-import java.util.ArrayList;
 
 
 /**
@@ -21,42 +16,12 @@ import java.util.ArrayList;
  */
 
 
-
-
 public class main {
     
-    
-static ArrayList<String>  readConfig(){
- ArrayList<String>ProgrammfileNames = new ArrayList<String>();
-       try {
-		BufferedReader in = new BufferedReader(new FileReader("HalConf.txt"));
-		String zeile = null;
-               
-		while ((zeile = in.readLine()) != null) {
-			
-                    if (!zeile.equals("#HAL_Prozessoren :")){
-                            // System.out.println("Gelesene Zeile: " + zeile);
-                             String tmp = zeile;
-                             String [] splittLine =zeile.split(" ");
-                            // System.out.println("Einzelne : "+Arrays.toString(splittLine));
-                        
-                       //     System.out.println(splittLine[1]);
-                            ProgrammfileNames.add(splittLine[1]);
-                    }    
-                        
-                   }
-                
-                
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-        
-    
-     return ProgrammfileNames;
-}
-    
+   
 
     // "Sicherung" von Termin 3 - einfache Ausführung im Normlen Thread. Bei Benutzung rückgabe ergänzen !
+    /*
    public void singleExec(String [] args){
        
          // Zum Aufruf des Programmes im Debug modus "-d" angeben !
@@ -96,10 +61,8 @@ static ArrayList<String>  readConfig(){
        
    }
     
-    
-    
-    
-     
+    */
+   
     public static void main(String[] args) throws IOException {
      
         boolean debugMode = false;
@@ -108,34 +71,10 @@ static ArrayList<String>  readConfig(){
             debugMode=true;
         }
         
-        // Namen aller Programme für die Einzelnen HAls
-    ArrayList<String> ProgrammNames = readConfig();
-        
-    // Hier sind Alle HAl Objekte Gepeichert
-    ArrayList<HAL_Interpreter> HALS = new ArrayList<HAL_Interpreter>();
-    
-    // Hal init und in HAlS AraayList + Starten der Threads
-    for (int i=0;i<ProgrammNames.size();i++){
-        System.out.println(ProgrammNames.get(i));
-        File inputFile = new File(ProgrammNames.get(i));
-        HAL_Interpreter tmpHAL = new HAL_Interpreter(4, 4, inputFile, debugMode);
-        HALS.add(tmpHAL);
-        tmpHAL.run();
-        
-    }
-    
-        
-        /*
-       Für nächste Aufgabe:   4 HAL Bausteine erstellen, 2 mittlere, einen für eingabe und einen für ausgabe. Von IO_Oberklasse erben. 
-        implements Runnable, damit keine mehrfachvererbung. 
-        
-        */
-        
-        
- 
-        
-        
-        
+        HAL_OS OS = new HAL_OS(debugMode);
+        OS.initHALS();
+        OS.startHALS();
+     
       
     }
     
